8965d3f1cc7368c27a72e71c1e2bb16877e11658
Nikki Gyurova <nikupbg@gmail.com>
2013-10-27 14:34:14 +0200

    hardcode database

--- LinkedFMI_UI/Controllers/HomeController.cs
+++ LinkedFMI_UI/Controllers/HomeController.cs
@@ -12,15 +12,6 @@ namespace LinkedFMI_UI.Controllers
     {
         public ActionResult Index()
         {
-            var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
-            var uri = new Uri(uriString);
-            var connectionString = new SqlConnectionStringBuilder
-            {
-                DataSource = uri.Host,
-                InitialCatalog = uri.AbsolutePath.Trim('/'),
-                UserID = uri.UserInfo.Split(':').First(),
-                Password = uri.UserInfo.Split(':').Last(),
-            }.ConnectionString;
             return View();
         }
 
